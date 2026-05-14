de2a0f16c5538170133abe3989c365e1afb6a618
Papa2k13 <125880027+Papa2k13@users.noreply.github.com>
2026-05-14 14:39:44 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-
-
-exports.english = require('./english')
+module.exports = {
+...require('./adapters/JSONFile.js'),
+...require('./adapters/JSONFileSync.js'),
+...require('./adapters/LocalStorage.js'),
+...require('./adapters/Memory.js'),
+...require('./adapters/MemorySync.js'),
+...require('./adapters/TextFile.js'),
+...require('./adapters/TextFileSync.js'),
+...require('./Low.js'),
+...require('./LowSync.js'),
+}
